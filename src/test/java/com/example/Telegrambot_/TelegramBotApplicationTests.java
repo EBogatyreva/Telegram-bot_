4218f0600f68c1974ea90ce0570adb37110a1d62
L <EBogatyrevaPK@yandex.ru>
2022-08-10 20:45:12 +1200

    Initial commit

---
+++ src/test/java/com/example/Telegrambot_/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Telegrambot_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
